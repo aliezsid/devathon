5ba5298e174e97009a1629735a6a7aceb29af089
Siddharth Gelera <sidgroks@gmail.com>
2018-03-09 12:22:38 +0530

    mockdata, added images

--- dist/bundle.js
+++ dist/bundle.js
@@ -25993,8 +25993,26 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var mockData = [{
 	id: (0, _uniqueId2.default)(),
-	title: "",
+	title: "Lion",
 	images: ["https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?w=1260&h=750&auto=compress&cs=tinysrgb"],
+	price: "3,000,000",
+	offerPrice: "",
+	shippingPrice: "",
+	inventory: "",
+	productDescription: ""
+}, {
+	id: (0, _uniqueId2.default)(),
+	title: "Mountain",
+	images: ["https://images.pexels.com/photos/640781/pexels-photo-640781.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb", "https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?w=1260&h=750&auto=compress&cs=tinysrgb", "https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?w=1260&h=750&auto=compress&cs=tinysrgb"],
+	price: "3,000,000",
+	offerPrice: "2,00,000",
+	shippingPrice: "",
+	inventory: "",
+	productDescription: ""
+}, {
+	id: (0, _uniqueId2.default)(),
+	title: "",
+	images: ["https://images.pexels.com/photos/753994/pexels-photo-753994.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"],
 	price: "",
 	offerPrice: "",
 	shippingPrice: "",
@@ -26003,52 +26021,12 @@ var mockData = [{
 }, {
 	id: (0, _uniqueId2.default)(),
 	title: "",
-	images: ["https://images.pexels.com/photos/640781/pexels-photo-640781.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb", "https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?w=1260&h=750&auto=compress&cs=tinysrgb", "https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?w=1260&h=750&auto=compress&cs=tinysrgb"],
+	images: ["https://images.pexels.com/photos/681405/pexels-photo-681405.jpeg?h=350&auto=compress&cs=tinysrgb", "https://images.pexels.com/photos/97904/pexels-photo-97904.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"],
 	price: "",
 	offerPrice: "",
 	shippingPrice: "",
 	inventory: "",
 	productDescription: ""
-	// {
-	// 	id: uniqueId(),
-	// 	title: "",
-	// 	images: [],
-	// 	price: "",
-	// 	offerPrice: "",
-	// 	shippingPrice: "",
-	// 	inventory: "",
-	// 	productDescription: ""
-	// },
-	// {
-	// 	id: uniqueId(),
-	// 	title: "",
-	// 	images: [],
-	// 	price: "",
-	// 	offerPrice: "",
-	// 	shippingPrice: "",
-	// 	inventory: "",
-	// 	productDescription: ""
-	// },
-	// {
-	// 	id: uniqueId(),
-	// 	title: "",
-	// 	images: [],
-	// 	price: "",
-	// 	offerPrice: "",
-	// 	shippingPrice: "",
-	// 	inventory: "",
-	// 	productDescription: ""
-	// },
-	// {
-	// 	id: uniqueId(),
-	// 	title: "",
-	// 	images: [],
-	// 	price: "",
-	// 	offerPrice: "",
-	// 	shippingPrice: "",
-	// 	inventory: "",
-	// 	productDescription: ""
-	// }
 }];
 
 exports.default = mockData;
